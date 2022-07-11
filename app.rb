3c2a40c0fe900dfd77caf279542f2d4f5b317bb4
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-07-11 14:57:01 +0100

    Added parameters

--- app.rb
+++ app.rb
@@ -10,15 +10,9 @@ class Application < Sinatra::Base
 
   # Declares a route that responds to a request with:
   #  - a GET method
-  #  - the path /
-  get '/' do
-    # The code here is executed when a request is received,
-    # and need to send a response. 
-
-    # We can simply return a string which
-    # will be used as the response content.
-    # Unless specified, the response status code
-    # will be 200 (OK).
-    return 'Some response data'
+  #  - the path /hello?name=Leo
+  get '/hello' do
+    name = params[:name]
+    return "Hello #{name}"
   end
 end
